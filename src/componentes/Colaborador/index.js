1f16308d820bd642fb64dbc40ed2a6744fc16001
Matheus Gomes <matheus_mgc@yahoo.com.br>
2024-07-09 00:11:38 -0300

    aula card colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,16 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/gomestizado.png' alt='Matheus Gomes'/>
+        </div>
+
+        <div className='rodape'>
+            <h4>Matheus Gomes</h4>
+            <h5>Estagiário</h5>
+        </div>
+        
+    </div>)
+}
+export default Colaborador
